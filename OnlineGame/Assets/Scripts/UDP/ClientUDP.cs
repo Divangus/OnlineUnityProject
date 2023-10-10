d7a8e5aeb1d45987cb03bc2a9fa4b4bc5c0933d8
DarkAvanger <awesomekoala19@gmail.com>
2023-10-10 11:34:12 +0200

    UDP/TCP

--- OnlineGame/Assets/Scripts/UDP/ClientUDP.cs
+++ OnlineGame/Assets/Scripts/UDP/ClientUDP.cs
@@ -1,46 +1,18 @@
-using System;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class SimpleUdpClient
+public class ClientUDP : MonoBehaviour
 {
-    public static void Main()
+    // Start is called before the first frame update
+    void Start()
     {
-        byte[] data = new byte[1024];
-        string input, stringData;
-        IPEndPoint ipep = new IPEndPoint(
-                        IPAddress.Parse("10.0.103.15"), 9050);
+        
+    }
 
-        Socket server = new Socket(AddressFamily.InterNetwork,
-                       SocketType.Dgram, ProtocolType.Udp);
-
-
-        string welcome = "Hello, are you there?";
-        data = Encoding.ASCII.GetBytes(welcome);
-        server.SendTo(data, data.Length, SocketFlags.None, ipep);
-
-        IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
-        EndPoint Remote = (EndPoint)sender;
-
-        data = new byte[1024];
-        int recv = server.ReceiveFrom(data, ref Remote);
-
-        Console.WriteLine("Message received from {0}:", Remote.ToString());
-        Console.WriteLine(Encoding.ASCII.GetString(data, 0, recv));
-
-        while (true)
-        {
-            input = Console.ReadLine();
-            if (input == "exit")
-                break;
-            server.SendTo(Encoding.ASCII.GetBytes(input), Remote);
-            data = new byte[1024];
-            recv = server.ReceiveFrom(data, ref Remote);
-            stringData = Encoding.ASCII.GetString(data, 0, recv);
-            Console.WriteLine(stringData);
-        }
-        Console.WriteLine("Stopping client");
-        server.Close();
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
