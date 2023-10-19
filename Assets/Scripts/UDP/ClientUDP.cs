b468b3346fe9b42ecd7b838a1e0e0b04f5425caf
Marina Albalà <marinalbala07@gmail.com>
2023-10-19 20:00:35 +0200

    udp chat

--- Assets/Scripts/UDP/ClientUDP.cs
+++ Assets/Scripts/UDP/ClientUDP.cs
@@ -11,17 +11,23 @@ public class ClientUDP : MonoBehaviour
 {
     Socket newSocket;
     byte[] data = new byte[1024];
+    byte[] message = new byte[1024];
     int recv;
     string stringData;
+
     IPEndPoint ipep;
-    // Start is called before the first frame update
+    
+
+    string strMessage = "";
+    int port = 9050;
+
     void Start()
     {
         Debug.Log("Start");
 
         Thread listenThread;
 
-        ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050);
+        ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
 
         newSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
 
@@ -35,15 +41,51 @@ public class ClientUDP : MonoBehaviour
         string welcome = "Hello, are you there?";
         data = Encoding.ASCII.GetBytes(welcome);
         newSocket.SendTo(data, data.Length, SocketFlags.None, ipep);
+        while (true)
+        {          
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+            EndPoint Remote = (EndPoint)sender;
 
-        IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
-        EndPoint Remote = (EndPoint)sender;
+            data = new byte[1024];
+            recv = newSocket.ReceiveFrom(data, ref Remote);
 
-        data = new byte[1024];
-        recv = newSocket.ReceiveFrom(data, ref Remote);
+            Debug.Log("Message received from:" + Remote.ToString());
+            Debug.Log(Encoding.ASCII.GetString(data, 0, recv));
+        }       
+        
+    }
 
-        Debug.Log("Message received from:"+ Remote.ToString());
-        Debug.Log(Encoding.ASCII.GetString(data, 0, recv));
+    private void OnDestroy()
+    {
         newSocket.Close();
+    }
+    // OnGUI
+    void OnGUI()
+    {
+        Rect rectObj = new Rect(40, 380, 200, 400);
+        GUIStyle style = new GUIStyle();
+        style.alignment = TextAnchor.UpperLeft;
+        GUI.Box(rectObj, "# UDPSend-Data\n127.0.0.1 " + port + " #\n"
+                    + "shell> nc -lu 127.0.0.1  " + port + " \n"
+                , style);
+
+        strMessage = GUI.TextField(new Rect(40, 420, 140, 20), strMessage);
+        if (GUI.Button(new Rect(190, 420, 40, 20), "send"))
+        {
+            SendString(strMessage + "\n");
+        }
+    }
+
+    private void SendString(string v)
+    {
+        try
+        {
+            message = Encoding.ASCII.GetBytes(v);
+            newSocket.SendTo(message, message.Length, SocketFlags.None, ipep);
+        }
+        catch (Exception err)
+        {
+            print(err.ToString());
+        }
     }
 }
--- Assets/Scripts/UDP/ServerUDP.cs
+++ Assets/Scripts/UDP/ServerUDP.cs
@@ -11,11 +11,15 @@ using System.Threading;
 public class ServerUDP : MonoBehaviour
 {
 
+    public string lastReceivedUdpPacket = "";
+    public string allReceivedUdpPacket = "";
+
     Socket newSocket;
     // Start is called before the first frame update
     int port = 9050;
     byte[] data = new byte[1024];
     int recv;
+    EndPoint Remote;
 
     void Start()
     {
@@ -35,14 +39,41 @@ public class ServerUDP : MonoBehaviour
 
     private void ReceiveData()
     {
-        Debug.Log("Receive");
-        IPEndPoint sender = new IPEndPoint(IPAddress.Any, port);
-        EndPoint Remote = (EndPoint)(sender);
-        recv = newSocket.ReceiveFrom(data, ref Remote);
+        
 
-        string welcome = "Welcome to my test server";
-        data = Encoding.ASCII.GetBytes(welcome);
-        newSocket.SendTo(data, data.Length, SocketFlags.None, Remote);
-        //newSocket.Close();
+        while (true)
+        {
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, port);
+            Remote = (EndPoint)(sender);
+            string welcome = "Welcome to my test server";
+            data = Encoding.ASCII.GetBytes(welcome);
+            newSocket.SendTo(data, data.Length, SocketFlags.None, Remote);
+            //newSocket.Close();
+            Debug.Log("Receive");          
+            
+            recv = newSocket.ReceiveFrom(data, ref Remote);
+
+            string text = Encoding.UTF8.GetString(data);
+
+            print(">> " + text);
+
+            lastReceivedUdpPacket = text;
+
+            allReceivedUdpPacket = allReceivedUdpPacket + text;
+        }            
     }
+
+    // OnGUI
+    void OnGUI()
+    {
+        Rect rectObj = new Rect(40, 10, 200, 400);
+        GUIStyle style = new GUIStyle();
+        style.alignment = TextAnchor.UpperLeft;
+        GUI.Box(rectObj, "# UDPReceive\n127.0.0.1 " + port + " #\n"
+                    + "shell> nc -u 127.0.0.1 : " + port + " \n"
+                    + "\nLast Packet: \n" + lastReceivedUdpPacket
+                    + "\n\nAll Messages: \n" + allReceivedUdpPacket
+                , style);
+    }
+
 }
